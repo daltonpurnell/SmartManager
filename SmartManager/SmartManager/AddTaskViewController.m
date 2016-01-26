9dea0bda515393454a122d2036c1cafebeee5463
daltonpurnell <daltonpurnell@live.com>
2016-01-26 14:45:51 -0700

    more textview delegate methods

--- SmartManager/SmartManager/AddTaskViewController.m
+++ SmartManager/SmartManager/AddTaskViewController.m
@@ -305,6 +305,20 @@
     [textView setText:@""];
 }
 
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    
+    if ([text isEqualToString:@"\n"]) {
+        
+        [textView resignFirstResponder];
+        // Return FALSE so that the final '\n' character doesn't get added
+        return NO;
+    }
+    // For any other character return TRUE so that the text gets added to the view
+    return YES;
+}
+
 /*
 #pragma mark - Navigation
 
